f700aa86ae03238dfec0d9174d825ee63cb0a05b
gal <tanjhu@163.com>
2019-12-30 10:51:38 +0800

    fix null contraint

--- student/views.py
+++ student/views.py
@@ -1,19 +1,37 @@
 from django.http import HttpResponseRedirect
-from django.urls import reverse
 from django.shortcuts import render
-
-from .models import Student
+from django.urls import reverse
+from django.views import View
 
 from .forms import StudentForm
+from .models import Student
 
 
-def index(request):
-    students = Student.objects.all()
-    if request.method == 'POST':
+class IndexView(View):
+    template_name = 'index.html'
+
+    def get_context(self):
+        students = Student.objects.all()
+        context = {
+            'students': students,
+        }
+        return context
+
+    def get(self, request):
+        context = self.get_context()
+        form = StudentForm()
+        context.update({
+            'form': form
+        })
+        #raise Exception('errorrrr')
+        response = render(request, self.template_name, context=context)
+        return response
+
+    def post(self, request):
         form = StudentForm(request.POST)
         if form.is_valid():
             cleaned_data = form.cleaned_data
-            dent = Student()
+            student = Student()
             student.name = cleaned_data['name']
             student.sex = cleaned_data['sex']
             student.email = cleaned_data['email']
@@ -22,11 +40,12 @@ def index(request):
             student.phone = cleaned_data['phone']
             student.save()
             return HttpResponseRedirect(reverse('index'))
-    else:
-       form = StudentForm()
-    context = {
-        'students': students,
-        'form': form,
-    }
-    return render(request, 'index.html', context=context)
+        context = self.get_context()
+        context.update({
+            'form': form
+        })
+        return render(request, self.template_name, context=context)
 
+
+class PrettyView(IndexView):
+    template_name = 'pretty.html'
